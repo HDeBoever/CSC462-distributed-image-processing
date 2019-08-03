eb01e083fe2ec23693b936142b29be60d84bcb02
HDeBoever <henrideboever@gmail.com>
2019-08-03 15:25:12 -0700

    sort images before processing

--- yolo-object-detection/processing_script.py
+++ yolo-object-detection/processing_script.py
@@ -6,10 +6,16 @@ import os, os.path, sys
 docker_variable = os.getenv('ID')
 print("docker var: " + docker_variable)
 
-for filename in os.listdir('split_images'):
-	curr_file_number = 0
+images = [f for f in listdir('split_images') if isfile(join('split_images', f))]
+images.sort(key = lambda x: int(''.join(filter(str.isdigit, x))))
+
+curr_file_number = 0
+for filename in images:
 
 	prefix = filename[:2]
 	if docker_variable == filename[0]:
 		os.system('python yolo.py --image split_images/' + prefix + 'airport' + str(curr_file_number + 1) +'.jpg --yolo yolo-coco')
+	else:
+		continue
+
 	curr_file_number += 1
