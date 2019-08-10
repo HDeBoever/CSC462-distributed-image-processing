c92dde8e5cba3eccc6e1ee05d718e3f8d9582a06
HDeBoever <henrideboever@gmail.com>
2019-08-10 13:44:26 -0700

    added mutltithread functionality

--- yolo-object-detection/processing_script.py
+++ yolo-object-detection/processing_script.py
@@ -6,13 +6,12 @@ import os, os.path, sys
 docker_variable = os.getenv('ID')
 print("docker var: " + docker_variable)
 
-curr_file_number = 0
 for filename in os.listdir('split_images'):
 
 	prefix = filename[:2]
+	# A_airport____.jpg
+	file_number = [9:-4]
 	if docker_variable == filename[0]:
-		os.system('python yolo.py --image split_images/' + prefix + 'airport' + str(curr_file_number + 1) +'.jpg --yolo yolo-coco')
+		os.system('python yolo.py --image split_images/' + prefix + 'airport' + str(file_number) +'.jpg --yolo yolo-coco')
 	else:
 		continue
-
-	curr_file_number += 1
